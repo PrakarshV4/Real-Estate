0a714a8d12cd9debd30c57c3248e4a83c9985b07
Prakarsh Verma <prakarsh1248@gmail.com>
2024-02-22 20:29:08 +0530

    Created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema =new mongoose.Schema({
+    username: {
+        type: 'string',
+        required: true,
+        unique: true
+    },
+    email: {
+        type: 'string',
+        required: true,
+        unique: true
+    },
+    password: {
+        type : 'string',
+        required: true,
+        unique: true
+    }
+},{
+    timestamps: true
+}) //to save time of creation and time of updation
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
