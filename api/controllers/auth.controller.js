53036f25a4e073e41c447907d3800ddeb95513d1
Prakarsh Verma <prakarsh1248@gmail.com>
2024-02-26 17:39:20 +0530

    Created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js'
 import bcyrptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import { errorHandler } from '../utils/error.js';
 export const signup = async(req,res,next)=>{
     //info from req.body
@@ -13,5 +14,22 @@ export const signup = async(req,res,next)=>{
         console.log(err);
         // next(errorHandler(550,"Error from the function"));
         next(err);
+    }
+}
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body;
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser) return next(errorHandler(404,"User not found"));
+        const validPassword = bcyrptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next (errorHandler(401,"Wrong credentials!"));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const {password: pass ,...rest} = validUser._doc;
+        // removing password from the data
+        res.cookie('access_token',token,{httpOnly:true})
+            .status(200)
+            .json(rest);  // sending user data without the password
+    } catch (error) {
+        next(error);
     }
 }
