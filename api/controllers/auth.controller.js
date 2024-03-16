281b770dfe16e3c97ce28c8e6d9b3893f4c9e81a
Prakarsh Verma <prakarsh1248@gmail.com>
2024-03-16 15:57:08 +0530

    Debugged some error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,6 +38,7 @@ export const signin = async(req,res,next)=>{
 export const google =  async(req, res, next) => {
     try{
         const user = await User.findOne({ email: req.body.email })
+        //if user exists create a token and send it in cookies
         if(user){
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
             const { password: pass, ...rest } = user._doc;
@@ -45,19 +46,24 @@ export const google =  async(req, res, next) => {
             .status(200)
             .json(rest)
         }else{
+            //since user does not exist in the db create a new user with generated password then send token and cookie
+            //generate pass as in schema we defined that we will receive a password
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashPassword = bcyrptjs.hashSync(generatedPassword,10);
+            //create newUser
             const newUser = new User({
                 username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4),
                 email: req.body.email,
                 password: hashPassword,
                 avatar: req.body.photo
             })
+            await newUser.save();
+            
             const token = jwt.sign({id: newUser._id},process.env.JWT_SECRET)
             const {password:pass , ...rest} = newUser._doc;
             res.cookie('access_token',token,{httpOnly: true}).status(200).json(rest); 
         }
     }catch(error) {
-
+        next(error);
     }
 }
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,21 +2,21 @@ import mongoose from "mongoose";
 
 const userSchema =new mongoose.Schema({
     username: {
-        type: 'string',
+        type: String,
         required: true,
         unique: true
     },
     email: {
-        type: 'string',
+        type: String,
         required: true,
         unique: true
     },
     password: {
-        type : 'string',
+        type : String,
         required: true,
     },
     avatar:{
-        type: 'string',
+        type: String,
         default: "https://t4.ftcdn.net/jpg/00/64/67/27/360_F_64672736_U5kpdGs9keUll8CRQ3p3YaEv2M6qkVY5.jpg"
     }
 },{
