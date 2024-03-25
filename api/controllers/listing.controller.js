56891c0e4f976440783903072a8cf776ce197daf
Prakarsh Verma <prakarsh1248@gmail.com>
2024-03-25 22:35:16 +0530

    Complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -4,6 +4,7 @@ import { errorHandler } from "../utils/error.js";
 export const createListing = async(req,res,next) => {
     try {
         const listing = await Listing.create(req.body);
+        console.log("listing="+listing); 
         return res.status(201).json(listing);
     } catch (error) {
         next(error);
@@ -28,20 +29,30 @@ export const deleteListing = async(req,res,next) => {
 export const updateListing = async(req,res,next)=>{
     const listing = await Listing.findById(req.params.id);
      if(!listing) return next(errorHandler(404,'Listing not found'));
-    //  console.log("req.user.id = "+req.user.id)
-    //  console.log("listing.userRef = "+listing.userRef)
+     
      if(req.user.id !== listing.userRef){
         return next(errorHandler(401,'You can edit your own listing'))
      }
 
      try {
-        const updateListing = Listing.findByIdAndUpdate(
+        const updateListing = await Listing.findByIdAndUpdate(
             req.params.id,
             req.body,
             {new: true}
         );
-        re.status(200).json(updateListing);
+        res.status(200).json(updateListing);
      } catch (error) {
         next(error);
      }
+}
+export const getListing = async(req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if(!listing){
+            return next(errorHandler(404, "Listing not found"));
+        }
+        res.status(200).json(listing);
+    } catch (error) {
+        next(error);
+    }
 }
