f5f2ed3a0a5a03e7bc6bbd0a252db3eaf2ed9a02
Prakarsh Verma <prakarsh1248@gmail.com>
2024-03-28 00:11:47 +0530

    Create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -56,3 +56,48 @@ export const getListing = async(req, res, next) => {
         next(error);
     }
 }
+export const getListings = async (req, res, next) => {
+    try {
+        
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer; //can be true,false or undefined
+        if(offer === undefined || offer ==='false'){
+            offer = { $in: [false,true] };
+        }
+        let furnished = req.query.furnished;
+        if(furnished === undefined || furnished === 'false'){
+            furnished = { $in: [true,false] };
+        }
+
+        let parking = req.query.parking;
+        if(parking === undefined || parking === 'false'){
+            parking = { $in: [true,false] };
+        }
+
+        const type = req.query.type;
+        if(type === undefined || type === 'all'){
+            type = {$in: ['sale','rent']};
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm , $options: 'i' }, //regx is built in search functionality for mongoDb  
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort(
+            { [sort]: order }
+        ).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+}
