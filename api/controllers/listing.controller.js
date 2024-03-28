8215f96eeff5e46e8de4658de86ccadd04654311
Prakarsh Verma <prakarsh1248@gmail.com>
2024-03-29 00:16:15 +0530

    Complete header search form

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -12,14 +12,14 @@ export const createListing = async(req,res,next) => {
 }
 export const deleteListing = async(req,res,next) => {
     // if(req.params.id !== req.)
+    try {
     const listing = await Listing.findById(req.params.id);
     if(!listing){
         return next(errorHandler(404,"Listing not found!"));
     }
     if(req.user.id !== listing.userRef){
-        return next(errorHandler(401,"You can only delete ypur own listings!"));
+        return next(errorHandler(401,"You can only delete your own listings!"));
     }
-    try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json("Listing has been deleted");
     } catch (error) {
@@ -27,6 +27,7 @@ export const deleteListing = async(req,res,next) => {
     } 
 }
 export const updateListing = async(req,res,next)=>{
+    try {
     const listing = await Listing.findById(req.params.id);
      if(!listing) return next(errorHandler(404,'Listing not found'));
      
@@ -34,7 +35,6 @@ export const updateListing = async(req,res,next)=>{
         return next(errorHandler(401,'You can edit your own listing'))
      }
 
-     try {
         const updateListing = await Listing.findByIdAndUpdate(
             req.params.id,
             req.body,
@@ -61,10 +61,12 @@ export const getListings = async (req, res, next) => {
         
         const limit = parseInt(req.query.limit) || 9;
         const startIndex = parseInt(req.query.startIndex) || 0;
+        
         let offer = req.query.offer; //can be true,false or undefined
         if(offer === undefined || offer ==='false'){
             offer = { $in: [false,true] };
         }
+        
         let furnished = req.query.furnished;
         if(furnished === undefined || furnished === 'false'){
             furnished = { $in: [true,false] };
@@ -87,7 +89,7 @@ export const getListings = async (req, res, next) => {
         const order = req.query.order || 'desc';
 
         const listings = await Listing.find({
-            name: { $regex: searchTerm , $options: 'i' }, //regx is built in search functionality for mongoDb  
+            name: { $regex: searchTerm , $options: 'i' }, //regx or regular expression is built in search functionality for mongoDb  
             offer,
             furnished,
             parking,
