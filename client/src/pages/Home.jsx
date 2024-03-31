587e70676a0fc4ed1c35babeff9bba0ccec5fcd5
Prakarsh Verma <prakarsh1248@gmail.com>
2024-03-31 22:55:36 +0530

    Added key to Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -69,7 +69,7 @@ export default function Home() {
       <Swiper navigation>
         {offerListings && offerListings.length > 0 && (
           offerListings.map((listing)=>(
-            <SwiperSlide>
+            <SwiperSlide key={listing._id}>
               <div style={{background:`url(${listing.imageUrls[0]}) center no-repeat` , backgroundSize:'cover'}}className='h-[500px]' key={listing._id}></div>
             </SwiperSlide>
           ))
